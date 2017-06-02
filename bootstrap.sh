fc980516c922144db1e56171a3878664c6536a63
Federico Paolantoni <federico@ASUS-BU210LA>
2017-06-03 00:57:28 +0200

    adding current user (vagrant) to docker group

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-apt-get remove -y docker docker-engine
+
 apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y docker-ce
+apt-get install -y docker-ce 
+# adding user vagrant to docker group
+sudo usermod -aG docker $USER
+#todo: install docker compose and docker swarm.
